a161118e5f4a6be2fd4390913f133fed776a38f8
imdixizz1 <134356220+imdixizz@users.noreply.github.com>
2025-04-13 10:51:25 +0530

    /

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ import { Actions } from "../src/components/provider/ApiKeys/reducer";
 import { filterTags } from "../src/utils/functions/index";
 import videoJson from "../src/components/videos";
 import { BannerText, HeroBanner } from "../src/components/tags/styles";
-import banner from "../public/banner.jpg";
+// import banner from "../public/images.png";
 
 export default function Home() {
   const [videos, setVideos] = useState([]);
@@ -95,7 +95,7 @@ export default function Home() {
         <div style={{ flex: 1, overflowY: "auto", paddingRight: "1rem" }}>
           <HeroBanner>
             <img
-              src={banner.src}
+              src="/images.png"
               alt="Music Banner"
               style={{ width: "100%", objectFit: "cover" }}
             />
